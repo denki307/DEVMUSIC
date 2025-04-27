77a347eb3368a9dce0ba8ac83c5d67a040883edc
denki307 <mbtdenki@gmail.com>
2025-04-27 07:43:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MUSIC_BOT_WORLD")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/JALLI_KATTU_KALA143")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 
 # make your bots privacy from telegra.ph and put your url here 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-AviaxMusic-08-14")
